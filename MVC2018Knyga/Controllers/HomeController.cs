ecf0b990d18d4fd98ef1acffd75b002a05033a10
donatas.dajoras <donatasdaj@gmail.com>
2018-09-25 16:58:06 +0300

    5 skyrius pradzia 1

--- MVC2018Knyga/Controllers/HomeController.cs
+++ MVC2018Knyga/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC2018Knyga.Models;
 
 namespace MVC2018Knyga.Controllers
 {
     public class HomeController : Controller
     {
+        private MusicStoreDB db = new MusicStoreDB();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +27,12 @@ namespace MVC2018Knyga.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+       
+        public ActionResult Search(string q)
+        {
+            var album = db.Albums.Include("Artist").Where(a => a.Title.Contains(q)).Take(10);
+            return View(album);
         }
     }
 }
